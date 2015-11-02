234b79fad2261bcf4a3e83f6a0750347001b3ecc
seo <seotownsend@icloud.com>
2015-11-01 19:51:33 -0500

    Added all binaries
    Former-commit-id: 0582365ab1b4368528a989386a332ffa8521d615

---
+++ Binaries/BUYPaymentButton.framework/Headers/BUYPaymentButton-umbrella.h
@@ -0,0 +1,7 @@
+#import <UIKit/UIKit.h>
+
+#import "BUYPaymentButton.h"
+
+FOUNDATION_EXPORT double BUYPaymentButtonVersionNumber;
+FOUNDATION_EXPORT const unsigned char BUYPaymentButtonVersionString[];
+
