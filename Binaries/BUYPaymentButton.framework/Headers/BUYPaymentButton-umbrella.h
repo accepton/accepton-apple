8a22bbbf1537ce6d2c409d6bb809442297b85dfb
seo <seotownsend@icloud.com>
2016-01-23 14:24:14 -0500

    build_universal fixes
    Former-commit-id: 84224a8cfe1d9b0e96f3995b8eb97b0c0549ea27

--- Binaries/BUYPaymentButton.framework/Headers/BUYPaymentButton-umbrella.h
+++
@@ -1,7 +0,0 @@
-#import <UIKit/UIKit.h>
-
-#import "BUYPaymentButton.h"
-
-FOUNDATION_EXPORT double BUYPaymentButtonVersionNumber;
-FOUNDATION_EXPORT const unsigned char BUYPaymentButtonVersionString[];
-
